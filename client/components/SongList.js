eece15c43f15940e412ee9417f714de6b84badd3
MaNaSDeo <rmnslogin@gmail.com>
2024-02-16 23:39:11 +0530

    Refetch Queries right way

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 import { Link } from "react-router";
+import query from "../queries/fetchSongs";
 
 class SongList extends Component {
   renderSongs() {
@@ -30,13 +31,13 @@ class SongList extends Component {
   }
 }
 
-const query = gql`
-  {
-    songs {
-      id
-      title
-    }
-  }
-`;
+// const query = gql`
+//   {
+//     songs {
+//       id
+//       title
+//     }
+//   }
+// `;
 
 export default graphql(query)(SongList);
